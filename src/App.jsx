fb9afcd9f3a2e9c19968665db052ff57ce4687c8
balaji242524 <balaji.g2022it@sece.ac.in>
2025-06-16 10:21:33 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+import React, { useState } from 'react';
+const App = () => {
+  const [team, setTeam] = useState([]);
+  const [tasks, setTasks] = useState([]);
+  return (
+    <>
+    </>
+
+
+  );
+}
